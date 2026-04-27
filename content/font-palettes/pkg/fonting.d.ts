6c1f9f5708022d7bf9025ba2dc5850fbb9ffb810
flowbish <flowbish@gmail.com>
2026-04-27 13:56:14 -0700

    add fonting wasm package to the font palette demo page. eventually, this will become a build artifact, probably, but for now it lives here

---
+++ content/font-palettes/pkg/fonting.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export class FontFilter {
+    free(): void;
+    [Symbol.dispose](): void;
+    generate_palette(buffer: Uint8Array): string;
+    constructor(filter: string);
+    parse_successful(): boolean;
+}
+
+export function greet(s: string): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_fontfilter_free: (a: number, b: number) => void;
+    readonly fontfilter_generate_palette: (a: number, b: number, c: number) => [number, number];
+    readonly fontfilter_new: (a: number, b: number) => number;
+    readonly fontfilter_parse_successful: (a: number) => number;
+    readonly greet: (a: number, b: number) => void;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ content/font-palettes/pkg/fonting_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_fontfilter_free: (a: number, b: number) => void;
+export const fontfilter_generate_palette: (a: number, b: number, c: number) => [number, number];
+export const fontfilter_new: (a: number, b: number) => number;
+export const fontfilter_parse_successful: (a: number) => number;
+export const greet: (a: number, b: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
